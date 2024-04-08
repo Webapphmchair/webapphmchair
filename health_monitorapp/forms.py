e257ff6a7b5f23d6a767431fd2bea0cf2adc4a60
Webapphmchair <165625333+Webapphmchair@users.noreply.github.com>
2024-04-08 19:36:19 +0800

    Update forms.py

--- health_monitorapp/forms.py
+++ health_monitorapp/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.forms import UserCreationForm 
-from .models import HealthRecord, CustomUser  # Add CustomUser import here
+from .models import HealthRecord, CustomUser, Comment # Add CustomUser import here
 
 # Password Reset  
 class CustomPasswordResetForm(PasswordResetForm):
@@ -12,6 +12,20 @@ class HealthRecordForm(forms.ModelForm):
     class Meta:
         model = HealthRecord
         fields = ['user_name', 'pulse_rate', 'heart_rate', 'blood_oxygen_level', 'body_temperature', 'height', 'body_weight']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']  # Remove 'health_record_id' from the fields
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget = forms.Textarea(attrs={'class': 'form-control', 'rows': 3})
+
+class EditCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
 
 # Create a new form for user registration with the approval feature
 class CustomUserCreationForm(UserCreationForm):
@@ -26,3 +40,4 @@ class CustomUserCreationForm(UserCreationForm):
             user.set_password(self.cleaned_data['password1']) 
             user.save()
         return user
+
