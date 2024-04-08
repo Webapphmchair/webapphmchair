c8c6f49386cc907e7a8111c192322a83476bd7d3
Webapphmchair <165625333+Webapphmchair@users.noreply.github.com>
2024-04-08 19:35:11 +0800

    Update views.py

--- health_monitorapp/views.py
+++ health_monitorapp/views.py
@@ -13,6 +13,10 @@ from django.contrib.auth import update_session_auth_hash
 from .models import HealthRecord
 from .forms import HealthRecordForm 
 from django.db.models import Case, When, F, FloatField, CharField, Value, ExpressionWrapper
+from .models import HealthRecord, Comment
+from .forms import CommentForm
+from django.db.models import Prefetch
+
 # Password View
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.models import User
@@ -41,6 +45,7 @@ logger = logging.getLogger(__name__)
 from .models import CustomUser  
 from .forms import CustomUserCreationForm  
 from django.contrib.admin.views.decorators import staff_member_required  
+from django.http import HttpResponseRedirect
 
 # Signup view
 from django.shortcuts import render, redirect
@@ -199,6 +204,25 @@ def health_records_list(request):
     if query:
         records = records.filter(user_name__icontains=query)
 
+    # Prefetch related comments to avoid additional database queries
+    records = records.prefetch_related(
+        Prefetch('comments', queryset=Comment.objects.select_related('user'))
+    )
+
+    # Handle comment submission
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            health_record_id = request.POST.get('health_record_id')
+            health_record = get_object_or_404(HealthRecord, pk=health_record_id)
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.health_record = health_record
+            comment.save()
+            return redirect('health_records_list')  # Redirect to refresh the page after submission
+    else:
+        comment_form = CommentForm()
+
     # Add interpretation for temperature
     records = records.annotate(
         body_temperature_status=Case(
@@ -250,21 +274,22 @@ def health_records_list(request):
             output_field=FloatField()
         ),
         bmi_status=Case(
-    When(bmi__lt=18.5, then=Value('Underweight')),
-    When(bmi__gte=18.5, bmi__lt=25, then=Value('Normal')),
-    When(bmi__gte=25, bmi__lt=30, then=Value('Overweight')),
-    When(bmi__gte=30, then=Value('Obese')),
-    default=Value('Abnormal'),
-    output_field=CharField()
-)
+            When(bmi__lt=18.5, then=Value('Underweight')),
+            When(bmi__gte=18.5, bmi__lt=25, then=Value('Normal')),
+            When(bmi__gte=25, bmi__lt=30, then=Value('Overweight')),
+            When(bmi__gte=30, then=Value('Obese')),
+            default=Value('Abnormal'),
+            output_field=CharField()
+        )
     )
 
     paginator = Paginator(records, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    
+
     context = {
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'comment_form': comment_form,
     }
     return render(request, 'health_monitorapp/health_records_list.html', context)
 
@@ -283,6 +308,7 @@ def add_health_record(request):
     return render(request, 'health_monitorapp/add_health_record.html', {'form': form})
 
 @login_required
+@staff_member_required
 def edit_health_record(request, pk):
     record = get_object_or_404(HealthRecord, pk=pk)
     if request.method == 'POST':
@@ -292,7 +318,7 @@ def edit_health_record(request, pk):
             updated_record.save()
             print("Health record updated successfully!")
             messages.success(request, 'Health record updated successfully!')
-            return redirect('health_records_list')  # Redirect to health records list upon successful update
+            return redirect(request.META.get('HTTP_REFERER', 'health_records_list'))  # Redirect to previous page
         else:
             print("Form is not valid:", form.errors)
             messages.error(request, 'Error occurred while updating health record. Please check the form.')
@@ -301,14 +327,68 @@ def edit_health_record(request, pk):
     return render(request, 'health_monitorapp/edit_health_record.html', {'form': form, 'record': record})
 
 @login_required
+@staff_member_required
 def delete_health_record(request, pk):
     record = get_object_or_404(HealthRecord, pk=pk)
     if request.method == 'POST':
         record.delete()
         messages.success(request, 'Health record deleted successfully!')
         # Redirect to health records list view after successful deletion
-        return redirect('health_records_list')
+        return redirect(request.META.get('HTTP_REFERER', 'health_records_list'))  # Redirect to previous page
     return render(request, 'health_monitorapp/delete_health_record.html', {'record': record})
+
+@login_required
+@staff_member_required
+def add_comment(request, health_record_id):
+    health_record = get_object_or_404(HealthRecord, pk=health_record_id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['text']
+            
+            # Create the comment
+            comment = Comment.objects.create(
+                health_record=health_record,
+                user=request.user,  # Assuming the current user is adding the comment
+                text=text
+            )
+            comment.save()
+
+            messages.success(request, 'Comment added successfully!')
+            return redirect('health_records_list')  # Redirect to the health record list page
+        else:
+            messages.error(request, 'Error occurred while adding comment. Please check the form.')
+    else:
+        form = CommentForm()
+
+    return render(request, 'health_monitorapp/health_records_list.html', {'form': form})
+
+@login_required
+@staff_member_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('health_records_list')  # Redirect to health_records_list page after successful edit
+    else:
+        form = CommentForm(instance=comment)  # Populate form with existing comment data
+    # Render health_records_list template with form and comment
+    return render(request, 'health_monitorapp/health_records_list.html', {'form': form, 'comment': comment})
+
+@login_required
+@staff_member_required
+def delete_comment(request, comment_id):
+    # Retrieve the comment object
+    comment = Comment.objects.get(pk=comment_id)
+    
+    if request.method == 'POST':
+        # Delete the comment
+        comment.delete()
+        # Redirect to the health records list or any other appropriate page
+        return redirect('health_records_list')
 
 @login_required
 def search_health_records(request):
